c3b557476e69efabbd072f52c578b55a4ee84e96
Sharynne Azhar <sharynneazhar@gmail.com>
2016-10-19 10:23:13 -0500

    reorganize somemore and make use of a template for the head

---
+++ eecs448/_templates/head.php
@@ -0,0 +1,20 @@
+<head>
+  <title><?php echo $title; ?></title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap CSS Plugin -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+    integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+    integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+  <link rel="stylesheet" href="css/main.css">
+
+  <!-- jQuery Plugin -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+
+  <!-- Bootstrap JS Plugin -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+    integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
--- eecs448/lab05/CreateUser.php
+++ eecs448/lab05/CreateUser.php
@@ -1,26 +1,6 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>EECS 448 | Lab 05</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS Plugin -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-      integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
-      integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-    <link rel="stylesheet" href="css/main.css">
-
-    <!-- jQuery Plugin -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-
-    <!-- Bootstrap JS Plugin -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
-      integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-  </head>
+  <?php $title = 'EECS 448 | Lab 05'; include('../_templates/head.php'); ?>
   <body>
     <nav class="navbar navbar-default">
       <div class="container">
@@ -47,14 +27,14 @@
 
         $user = $mysqli->real_escape_string($_POST["username"]);
 
-        if (!isset($user) || empty($user)) {
+        if (!isset($user) || !strlen(trim($user))) {
           echo 'Username cannot be empty<br />';
           exit();
         }
 
         $query = "INSERT INTO Users (user_id) VALUES ('" . $user . "')";
         if ($result = $mysqli->query($query)) {
-          printf("User %s successfully created!", $user);
+          printf("%s successfully created!", $user);
         } else {
           if ($mysqli->errno == 1062) {
             printf("User %s already exists!", $user);
