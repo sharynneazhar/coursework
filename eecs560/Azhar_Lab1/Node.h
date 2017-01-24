14a0f5a2bc9851ff3ba88c0c29a2f56732f927c9
Sharynne Azhar <sharynneazhar@gmail.com>
2017-01-24 12:22:21 -0600

    implement Node class

---
+++ eecs560/Azhar_Lab1/Node.h
@@ -0,0 +1,30 @@
+/**
+*	@file : Node.h
+*	@author : Sharynne Azhar
+*	@date : 01-24-2017
+*/
+
+#ifndef NODE_H
+#define NODE_H
+
+template <typename T>
+class Node {
+  private:
+    T m_value;
+    Node<T>* m_prev;
+    Node<T>* m_next;
+
+  public:
+    Node();
+    T getValue() const;
+    T setValue(T val);
+    Node<T>* getPrev() const;
+    void setPrev(Node<T>* prev);
+    Node<T>* getNext() const;
+    void setNext(Node<T>* next);
+
+};
+
+#include "Node.hpp"
+
+#endif
