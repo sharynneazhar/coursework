203b6f0aead593c6afcad58ae058b2f678359887
Sharynne Azhar <sharynneazhar@gmail.com>
2017-02-13 21:46:29 -0600

    finish 678 lab3 code portion

--- eecs678/lab3-ipc/finder.c
+++ eecs678/lab3-ipc/finder.c
@@ -6,6 +6,8 @@
 #include <sys/wait.h>
 
 #define BSIZE 256
+#define READ_END 0
+#define WRITE_END 1
 
 #define BASH_EXEC  "/bin/bash"
 #define FIND_EXEC  "/bin/find"
@@ -24,29 +26,114 @@ int main(int argc, char *argv[])
 		exit(0);
 	}
 
+	// open new file descriptors
+	int fd1[2], fd2[2], fd3[3];
+
+	// call pipes
+	// since we have four processes, we only need 3 pipes
+	pipe(fd1);
+	pipe(fd2);
+	pipe(fd3);
+
 	pid_1 = fork();
 	if (pid_1 == 0) {
 		/* First Child */
+		char cmdbuf[BSIZE];
+		bzero(cmdbuf, BSIZE);
+		sprintf(cmdbuf, "%s %s -name \'*\'.[ch]", FIND_EXEC, argv[1]);
+
+		dup2(fd1[WRITE_END], STDOUT_FILENO);
+
+		close(fd1[READ_END]);
+		close(fd1[WRITE_END]);
+		close(fd2[READ_END]);
+		close(fd2[WRITE_END]);
+		close(fd3[READ_END]);
+		close(fd3[WRITE_END]);
+
+		char* const args[] = {BASH_EXEC, "-c", cmdbuf, (char *) 0};
+		if (execv(BASH_EXEC, args)) {
+			fprintf(stderr, "\nError execing find. ERROR#%d\n", errno);
+			return EXIT_FAILURE;
+		}
+
 		exit(0);
 	}
 
 	pid_2 = fork();
 	if (pid_2 == 0) {
 		/* Second Child */
+
+		dup2(fd1[READ_END], STDIN_FILENO);
+		dup2(fd2[WRITE_END], STDOUT_FILENO);
+
+		close(fd1[READ_END]);
+		close(fd1[WRITE_END]);
+		close(fd2[READ_END]);
+		close(fd2[WRITE_END]);
+		close(fd3[READ_END]);
+		close(fd3[WRITE_END]);
+
+		char* const args[] = {XARGS_EXEC, GREP_EXEC, "-c", argv[2], (char *) 0};
+		if (execv(XARGS_EXEC, args)) {
+			fprintf(stderr, "\nError execing grep. ERROR#%d\n", errno);
+			return EXIT_FAILURE;
+		}
+
 		exit(0);
 	}
 
 	pid_3 = fork();
 	if (pid_3 == 0) {
 		/* Third Child */
+
+		dup2(fd2[READ_END], STDIN_FILENO);
+		dup2(fd3[WRITE_END], STDOUT_FILENO);
+
+		close(fd1[READ_END]);
+		close(fd1[WRITE_END]);
+		close(fd2[READ_END]);
+		close(fd2[WRITE_END]);
+		close(fd3[READ_END]);
+		close(fd3[WRITE_END]);
+
+		char* const args[] = {SORT_EXEC, "-t", ":", "+1.0", "-2.0", "--numeric", "--reverse", (char *) 0};
+		if (execv(SORT_EXEC, args)) {
+			fprintf(stderr, "\nError execing sort. ERROR#%d\n", errno);
+			return EXIT_FAILURE;
+		}
+
 		exit(0);
 	}
 
 	pid_4 = fork();
 	if (pid_4 == 0) {
 		/* Fourth Child */
+
+		dup2(fd3[READ_END], STDIN_FILENO);
+
+		close(fd1[READ_END]);
+		close(fd1[WRITE_END]);
+		close(fd2[READ_END]);
+		close(fd2[WRITE_END]);
+		close(fd3[READ_END]);
+		close(fd3[WRITE_END]);
+
+		char* const args[] = {HEAD_EXEC, "--lines", argv[3], (char *) 0};
+		if (execv(HEAD_EXEC, args)) {
+			fprintf(stderr, "\nError execing head. ERROR#%d\n", errno);
+			return EXIT_FAILURE;
+		}
+
 		exit(0);
 	}
+
+	close(fd1[READ_END]);
+	close(fd1[WRITE_END]);
+	close(fd2[READ_END]);
+	close(fd2[WRITE_END]);
+	close(fd3[READ_END]);
+	close(fd3[WRITE_END]);
 
 	if ((waitpid(pid_1, &status, 0)) == -1) {
 		fprintf(stderr, "Process 1 encountered an error. ERROR%d", errno);
--- eecs678/lab3-ipc/pipe.c
+++ eecs678/lab3-ipc/pipe.c
@@ -6,7 +6,9 @@
 #include <sys/wait.h>
 
 #define R_FILE "/proc/meminfo"
-#define BSIZE 256 
+#define BSIZE 256
+#define READ_END 0
+#define WRITE_END 1
 
 int main()
 {
@@ -16,14 +18,18 @@ int main()
 	/* XXX - need to declare pipes and make the pipe() system call before
 	 * forking any children
 	 */
+	 int fd[2];
+	 pipe(fd);
 
 	pid_1 = fork();
-	if (pid_1 == 0) { 
-		/* process a */ 
+	if (pid_1 == 0) {
+		/* process a */
 
 		int rfd;
 		size_t rsize;
 		char buf[BSIZE];
+
+		close(fd[READ_END]);
 
 		if ((rfd = open(R_FILE, O_RDONLY)) < 0) {
 			fprintf(stderr, "\nError opening file: %s. ERROR#%d\n", R_FILE, errno);
@@ -33,36 +39,41 @@ int main()
 		/* read contents of file and write it out to a pipe */
 		while ((rsize = read(rfd, buf, BSIZE)) > 0) {
 			/* XXX - this should write to a pipe - not to stdout */
-			write(STDOUT_FILENO, buf, rsize);
+			write(fd[WRITE_END], buf, rsize);
 		}
 
+		close(fd[WRITE_END]);
 		close(rfd);
-		return 0; 
-	} 
+		return 0;
+	}
 
 	pid_2 = fork();
 	if (pid_2 == 0) {
 		/* process b */
-#if 0
 		size_t rsize;
 		char buf[BSIZE];
 
-		/* read from pipe and write out contents to the terminal */
+		close(fd[WRITE_END]);
 
+		/* read from pipe and write out contents to the terminal */
 		/* XXX - this should read from a pipe - not from stdin */
-		while ((rsize = read(STDIN_FILENO, buf, BSIZE)) > 0) {
+		while ((rsize = read(fd[READ_END], buf, BSIZE)) > 0) {
 			write(STDOUT_FILENO, buf, rsize);
 		}
-#endif
+
+		close(fd[READ_END]);
 
 		return 0;
 	}
+
+	close(fd[WRITE_END]);
+	close(fd[READ_END]);
 
 	/* shell process */
 	if ((waitpid(pid_1, &status, 0)) == -1) {
 		fprintf(stderr, "Process 1 encountered an error. ERROR%d", errno);
 		return EXIT_FAILURE;
-	}        
+	}
 
 	if ((waitpid(pid_2, &status, 0)) == -1) {
 		fprintf(stderr, "Process 2 encountered an error. ERROR%d", errno);
