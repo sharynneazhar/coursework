f7947f0734dde59e61da3e7736af5b807a9f4ead
Sharynne Azhar <sharynneazhar@gmail.com>
2017-02-17 15:43:33 -0600

    this will do

--- eecs560/Azhar_Lab4/main.cpp
+++ eecs560/Azhar_Lab4/main.cpp
@@ -15,37 +15,8 @@
 
 const int DEFAULT_TABLE_SIZE = 600011;
 
-void prettyPrint(std::string hashMethod, float loadFactors[8],
-  double seedTimes[5][8], double averageTimes[8]) {
-  std::cout << std::endl;
-  std::cout << std::left << std::setw(118) << std::setfill('-') << "-";
-  std::cout << "\n" << hashMethod << "\n";
-  std::cout << std::left << std::setw(118) << std::setfill('-') << "-";
-
-  std::cout << std::left << std::setw(18) << std::setfill(' ') << "\nLoad Factor:";
-  for (int i = 0; i < 8; i++) {
-    std::cout << std::left << std::setw(13) << std::setfill(' ') << loadFactors[i];
-  }
-  std::cout << std::endl;
-  std::cout << std::left << std::setw(118) << std::setfill('-') << "-";
-
-  for (int i = 1; i <= 5; i++) {
-    std::cout << std::left << std::setw(18) << std::setfill(' ') << "\nSeed #";
-    for (int j = 0; j < 8; j++) {
-      std::cout << std::left << std::setw(13) << std::setfill(' ') << seedTimes[i][j];
-    }
-  }
-
-  std::cout << std::endl;
-  std::cout << std::left << std::setw(118) << std::setfill('-') << "-";
-  std::cout << std::left << std::setw(18) << std::setfill(' ') << "\nAverage Time:";
-  for (int i = 0; i < 8; i++) {
-    std::cout << std::left << std::setw(13) << std::setfill(' ') << averageTimes[i] / 5;
-  }
-
-  std::cout << std::endl;
-  std::cout << std::left << std::setw(118) << std::setfill('-') << "-" << std::endl;
-}
+// adds horizontal line
+void hr() { std::cout << std::left << std::setw(115) << std::setfill('-') << "-"; }
 
 int main(int argc, char* argv[]) {
 
@@ -53,73 +24,89 @@ int main(int argc, char* argv[]) {
   // before its capacity is automatically increased (i.e. rehashed)
   float loadFactors[8] = { 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9 };
 
-  // keep track of results from different load factors
-  double openHashLoadFactorTimes[8];
-  double quadraticLoadFactorTimes[8];
-  double doubleHashLoadFactorTimes[8];
-
-  // keep track result from 5 different seeds
-  double openHashSeedTimes[5][8];
-  double quadraticSeedTimes[5][8];
-  double doubleHashSeedTimes[5][8];
-
   // keep track average result from 5 different seeds
-  double openHashAverageTimes[8];
-  double quadraticAverageTimes[8];
-  double doubleHashAverageTimes[8];
+  double openHashTotalTime[8];
+  double quadraticTotalTime[8];
+  double doubleHashTotalTime[8];
 
   // initialize our timer
   Timer timer;
 
-  std::cout << "\n\n>> Calculating open hashing method...\n\n";
+  // run 5 different trials
+  std::cout << "\n\nOpen Hashing\n"; hr();
   for (int seedIdx = 1; seedIdx <= 5; seedIdx++) {
+    std::cout << "\nSeed #" << seedIdx << std::left << std::setw(5) << std::setfill(' ') << ":";
     srand(seedIdx);
     timer.start();
     for (int i = 0; i < 8; i++) {
-      // calculate how many elements we can insert based on load factor
       int numElements = floor(DEFAULT_TABLE_SIZE * loadFactors[i]);
       OpenHash<long>* openHash = new OpenHash<long>(numElements);
       for (int j = 0; j < numElements; j++) {
-        long num = rand() % 2147483647L; // need L at the end to preserve long
-        openHash->insertValue(num);
+        openHash->insertValue(rand() % 2147483647L);
       }
       double _time = timer.stop();
-      openHashSeedTimes[seedIdx][i] += _time;
-      openHashAverageTimes[i] += openHashSeedTimes[seedIdx][i];
+      std::cout << std::left << std::setw(13) << std::setfill(' ') << _time;
+      openHashTotalTime[i] += _time;
       delete openHash;
     }
   }
 
-  prettyPrint("Open Hashing", loadFactors, openHashSeedTimes, openHashAverageTimes);
+  std::cout << std::endl; hr();
+  std::cout << "\nAverage" << std::left << std::setw(5) << std::setfill(' ') << ":";
+  for (int i = 0; i < 8; i++) {
+    std::cout << std::left << std::setw(13) << std::setfill(' ') << openHashTotalTime[i] / 5;
+  }
 
-  // std::cout << "\n>> Calculating quadratic probing method...\n\n";
-  // timer.start();
-  // for (int i = 0; i < 8; i++) {
-  //   int numElements = floor(DEFAULT_TABLE_SIZE * loadFactors[i]);
-  //   ClosedHash<long>* quadraticHash = new ClosedHash<long>(numElements, 'Q');
-  //   for (int j = 0; j < numElements; j++) {
-  //     long num = rand() % 2147483647L; // need L at the end to preserve long
-  //     quadraticHash->insertValue(num);
-  //   }
-  //   quadraticLoadFactorTimes[i] = timer.stop();
-  //   delete quadraticHash;
-  // }
-  // prettyPrint("Quadratic Probing", loadFactors, quadraticLoadFactorTimes, 8);
-  //
-  // std::cout << "\n>> Calculating double hashing method...\n\n";
-  // timer.start();
-  // for (int i = 0; i < 8; i++) {
-  //   int numElements = floor(DEFAULT_TABLE_SIZE * loadFactors[i]);
-  //   ClosedHash<long>* doubleHash = new ClosedHash<long>(numElements, 'D');
-  //   for (int j = 0; j < numElements; j++) {
-  //     long num = rand() % 2147483647L; // need L at the end to preserve long
-  //     doubleHash->insertValue(num);
-  //   }
-  //   doubleHashLoadFactorTimes[i] = timer.stop();
-  //   delete doubleHash;
-  // }
-  // prettyPrint("Double Hashing", loadFactors, doubleHashLoadFactorTimes, 8);
+  std::cout << "\n\n\nQuadratic Probing\n"; hr();
+  for (int seedIdx = 1; seedIdx <= 5; seedIdx++) {
+    std::cout << "\nSeed #" << seedIdx << std::left << std::setw(5) << std::setfill(' ') << ":";
+    srand(seedIdx);
+    timer.start();
+    for (int i = 0; i < 8; i++) {
+      int numElements = floor(DEFAULT_TABLE_SIZE * loadFactors[i]);
+      ClosedHash<long>* quadraticHash = new ClosedHash<long>(numElements, 'Q');
+      for (int j = 0; j < numElements; j++) {
+        quadraticHash->insertValue(rand() % 2147483647L);
+      }
+      double _time = timer.stop();
+      std::cout << std::left << std::setw(13) << std::setfill(' ') << _time;
+      quadraticTotalTime[i] += _time;
+      delete quadraticHash;
+    }
+  }
 
+  std::cout << std::endl; hr();
+  std::cout << "\nAverage" << std::left << std::setw(5) << std::setfill(' ') << ":";
+  for (int i = 0; i < 8; i++) {
+    std::cout << std::left << std::setw(13) << std::setfill(' ') << quadraticTotalTime[i] / 5;
+  }
+
+  std::cout << "\n\n\nDouble Hashing\n"; hr();
+  for (int seedIdx = 1; seedIdx <= 5; seedIdx++) {
+    std::cout << "\nSeed #" << seedIdx << std::left << std::setw(5) << std::setfill(' ') << ":";
+    srand(seedIdx);
+    timer.start();
+    for (int i = 0; i < 8; i++) {
+      int numElements = floor(DEFAULT_TABLE_SIZE * loadFactors[i]);
+      ClosedHash<long>* doubleHash = new ClosedHash<long>(numElements, 'D');
+      for (int j = 0; j < numElements; j++) {
+        doubleHash->insertValue(rand() % 2147483647L);
+      }
+      double _time = timer.stop();
+      std::cout << std::left << std::setw(13) << std::setfill(' ') << _time;
+      doubleHashTotalTime[i] += _time;
+      delete doubleHash;
+    }
+  }
+
+  std::cout << std::endl; hr();
+  std::cout << "\nAverage" << std::left << std::setw(5) << std::setfill(' ') << ":";
+  for (int i = 0; i < 8; i++) {
+    std::cout << std::left << std::setw(13) << std::setfill(' ') << doubleHashTotalTime[i] / 5;
+  }
+
+
+  std::cout << "\n\nAll done!\n\n";
 
   return 0;
 };
