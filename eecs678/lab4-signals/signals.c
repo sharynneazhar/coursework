f4d77aad18d8b8833972e998036cb4e632753be8
Sharynne Azhar <sharynneazhar@gmail.com>
2017-02-19 12:02:46 -0600

    finish 678 lab4

--- eecs678/lab4-signals/signals.c
+++ eecs678/lab4-signals/signals.c
@@ -21,6 +21,10 @@ void catch_int(int sig_num)
     printf("\nReally exit? [Y/n]: ");
     fflush(stdout);
     fgets(answer, sizeof(answer), stdin);
+
+    // alarm if user is taking too long
+    alarm(8);
+
     if (answer[0] == 'n' || answer[0] == 'N') {
       printf("\nContinuing\n");
       fflush(stdout);
@@ -45,14 +49,39 @@ void catch_tstp(int sig_num)
   fflush(stdout);
 }
 
+/* Timeout to exit the program if the user takes too long */
+void catch_alarm(int sig_num) 
+{
+  printf("\n\nUser taking too long to respond. Exiting...\n\n");
+  fflush(stdout);
+  exit(0);
+}
+
 int main(int argc, char* argv[])
 {
   struct sigaction sa;
   sigset_t mask_set;  /* used to set a signal masking set. */
 
   /* setup mask_set */
+  sigfillset(&mask_set);
 
   /* set signal handlers */
+  sigdelset(&mask_set, SIGALRM);
+  sa.sa_mask = mask_set;
+  sa.sa_handler = catch_int;
+  sigaction(SIGINT, &sa, NULL);
+
+  sa.sa_handler = catch_tstp;
+  sigaction(SIGTSTP, &sa, NULL);
+
+  sa.sa_handler = catch_alarm;
+  sigaction(SIGALRM, &sa, NULL);
+
+  /* causes the calling process to wait until any signal is received
+   * - must be called in a loop */
+  while (1) {
+    pause();
+  }
 
   return 0;
 }
