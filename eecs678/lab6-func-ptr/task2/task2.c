e68ba8c27c791d76c0af67a6d737a71a48564bc1
Sharynne Azhar <sharynneazhar@gmail.com>
2017-03-15 09:54:49 -0500

    finish task 2

--- eecs678/lab6-func-ptr/task2/task2.c
+++ eecs678/lab6-func-ptr/task2/task2.c
@@ -1,15 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* IMPLEMENT ME: Declare your functions here */
 int add (int a, int b);
+int subtract (int a, int b);
+int multiply (int a, int b);
+int divide (int a, int b);
+
+int (*op[4]) (int a, int b);
 
 int main (void)
 {
-	/* IMPLEMENT ME: Insert your algorithm here */
+	int operation;
+
+	printf("Operand 'a' : 6 | Operand 'b' : 3\n");
+	printf("Specify the operation to perform\n");
+	printf("(0 : Add | 1 : Subtract | 2 : Multiply | 3 : Divide): ");
+  scanf("%d", &operation);
+
+	// Note: &func and func is the same thing
+	op[0] = add;
+	op[1] = subtract;
+	op[2] = multiply;
+	op[3] = divide;
+
+	int result = (*op[operation])(6, 3);
+
+	printf("x = %d\n", result);
+
 
 	return 0;
 }
 
-/* IMPLEMENT ME: Define your functions here */
-int add (int a, int b) { printf ("Adding 'a' and 'b'\n"); return a + b; }
+int add (int a, int b) {
+	printf ("Adding 'a' and 'b'\n");
+	return a + b;
+}
+
+int subtract (int a, int b) {
+	printf ("Subtracting 'a' and 'b'\n");
+	return a - b;
+}
+
+int multiply (int a, int b) {
+	printf ("Multiplying 'a' and 'b'\n");
+	return a * b;
+}
+
+int divide (int a, int b) {
+	printf ("Dividing 'a' and 'b'\n");
+	return a / b;
+}
