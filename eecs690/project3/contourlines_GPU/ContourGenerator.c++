c58295805cb91a775f3e98e1e44eaff6cf36be18
Sharynne Azhar <sharynneazhar@gmail.com>
2017-04-25 12:49:53 -0500

    moving things around

--- eecs690/project3/contourlines_GPU/ContourGenerator.c++
+++ eecs690/project3/contourlines_GPU/ContourGenerator.c++
@@ -48,14 +48,23 @@ int ContourGenerator::computeContourEdgesFor(float level, vec2*& lines)
 	lines = new vec2[numActualPoints];
 	// Use CUDA or OpenCL code to retrieve the points, placing them into "lines".
 	// As a placeholder for now, we will just make an "X" over the area:
-	lines[0][0] = 0.0; lines[0][1] = 0.0;
-	lines[1][0] = nColsOfVertices - 1.0; lines[1][1] = nRowsOfVertices - 1.0;
-	lines[2][0] = 0.0; lines[2][1] = nRowsOfVertices - 1.0;
-	lines[3][0] = nColsOfVertices - 1.0; lines[3][1] = 0.0;
+	lines[0][0] = 0.0;
+	lines[0][1] = 0.0;
+
+	lines[1][0] = nColsOfVertices - 1.0;
+	lines[1][1] = nRowsOfVertices - 1.0;
+
+	lines[2][0] = 0.0;
+	lines[2][1] = nRowsOfVertices - 1.0;
+
+	lines[3][0] = nColsOfVertices - 1.0;
+	lines[3][1] = 0.0;
 
 	// After the line end points have been returned from the device, delete the
 	// device buffer to prevent a memory leak.
 	// ... do it here ...
+
+	
 
 	// return number of coordinate pairs in "lines":
 	return numActualPoints;
--- eecs690/project3/contourlines_GPU/ContourGenerator.h
+++ eecs690/project3/contourlines_GPU/ContourGenerator.h
@@ -4,19 +4,37 @@
 #ifndef CONTOURGENERATOR_H
 #define CONTOURGENERATOR_H
 
+/**************************************************************************
+ * System Includes
+ **************************************************************************/
 #include <iostream>
 #include <fstream>
 
+/**************************************************************************
+ * OpenCL Includes
+ **************************************************************************/
+#ifdef __APPLE__
+	#include <OpenCL/opencl.h>
+#else
+	#include <CL/opencl.h>
+#endif
+
+/**************************************************************************
+ * Public Types and Global Definitions
+ **************************************************************************/
 typedef float vec2[2];
 
+/**************************************************************************
+ * Contour Generator Class
+ **************************************************************************/
 class ContourGenerator
 {
 public:
 	ContourGenerator(std::istream& inp);
 	virtual ~ContourGenerator();
 
-    // Fires a GPU kernel to compute edges for the given level and return them
-    int computeContourEdgesFor(float level, vec2*& lines);
+  // Fires a GPU kernel to compute edges for the given level and return them
+  int computeContourEdgesFor(float level, vec2*& lines);
 
 	size_t getNumberCols() const { return nColsOfVertices; }
 	size_t getNumberRows() const { return nRowsOfVertices; }
