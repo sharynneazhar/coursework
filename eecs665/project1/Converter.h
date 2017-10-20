cf99954c7f4ac37dd1dfb97d6d3b40ceb0549c11
Sharynne Azhar <sharynneazhar@gmail.com>
2017-10-20 18:28:48 -0500

    [665:Project1] idk...

--- eecs665/project1/Converter.h
+++ eecs665/project1/Converter.h
@@ -20,28 +20,28 @@
 #include "Utils.h"
 
 class Converter {
-  private:
-    std::vector<int> epsilonClosure(std::vector<int> &, int);
-    void mark(std::vector<int>, int);
-    void getDFAInfo();
+private:
+  std::vector<int> epsilonClosure(std::vector<int> &, int);
+  void mark(std::vector<int>, int);
+  void getDFAInfo();
 
-  public:
-    Utils utils;
+public:
+  Utils utils;
 
-    int initialState = 0;
-    int dfaFinalStates = 0;
-    int numStates = 0;
-    int numStateSymbols = 0;
+  int initialState = 0;
+  int dfaFinalStates = 0;
+  int numStates = 0;
+  int numStateSymbols = 0;
 
-    std::vector<std::string> inputVector;
-    std::vector<int> finalStates;
-    std::vector<char> stateSymbols;
-    std::vector<std::string> nextTransitions;
-    std::vector<int> intNextTransitions;
-    std::vector<std::vector<int>> epsilonStates;
-    std::vector<std::vector<int>> dfaStates;
-    std::vector<int> dfaFinalVector;
-    std::vector<int> dfaTransitionNum;
+  std::vector<std::string> inputVector;
+  std::vector<int> finalStates;
+  std::vector<char> stateSymbols;
+  std::vector<std::string> nextTransitions;
+  std::vector<int> intNextTransitions;
+  std::vector<std::vector<int>> epsilonStates;
+  std::vector<std::vector<int>> dfaStates;
+  std::vector<int> dfaFinalVector;
+  std::vector<int> dfaTransitionNum;
 
   Converter(std::vector<std::string> vec);
   ~Converter();
@@ -52,6 +52,6 @@ class Converter {
   void printResults();
 };
 
-#include "converter.cpp"
+#include "Converter.cpp"
 
 #endif
