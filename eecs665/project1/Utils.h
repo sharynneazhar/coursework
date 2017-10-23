a2118cd5cc95c4fc34796f48d93b3622c15e0483
Sharynne Azhar <sharynneazhar@gmail.com>
2017-10-23 03:47:05 -0500

    shenanigans…

--- eecs665/project1/Utils.h
+++ eecs665/project1/Utils.h
@@ -16,88 +16,121 @@
 #include <cctype>
 #include <locale>
 #include <vector>
+#include <sstream>
+#include <queue>
+#include <set>
+#include <unordered_map>
 
 class Utils {
 private:
-  /**
-   * Trims whitespace off a string
-   * @param  str String to trim
-   * @return     Trimmed string with no whitespace
-   * @ref: https://stackoverflow.com/questions/216823/whats-the-best-way-to-trim-stdstring
-   */
-
-  // trim left end
+  // Trims whitespace on left end of string
   static std::string &leftTrim(std::string &str) {
     str.erase(str.begin(), std::find_if(str.begin(), str.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
     return str;
   }
 
-  // trim right end
+  // Trims whitespace on right end of string
   static std::string &rightTrim(std::string &str) {
     str.erase(std::find_if(str.rbegin(), str.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), str.end());
     return str;
   }
 
+  static void eClosureHelper(std::unordered_map<int, std::set<int>> transitions, std::set<int> states, std::set<int> &result) {
+    for (int state : states) {
+      if (result.find(state) == result.end()) {
+        result.insert(state);
+        eClosureHelper(transitions, transitions.find(state)->second, result);
+      }
+    }
+  }
+
 public:
-  /**
-   * Constructor
-   */
+  // Constructor
   Utils() {}
 
   // Trims whitespace
+  // @ref: https://stackoverflow.com/questions/216823/whats-the-best-way-to-trim-stdstring
   static std::string &trim(std::string &s) {
     return rightTrim(leftTrim(s));
   }
 
   // Finds a substring in a string contained within two specified delimiters
-  std::string findString(std::string str, std::string startDelimiter, std::string stopDelimiter) {
-    std::size_t startIndex = str.find(startDelimiter);
-    std::size_t stopIndex = str.find(stopDelimiter);
-    std::string target = str.substr(startIndex + 1, startIndex - stopIndex - 1);
+  static std::string trimBrackets(std::string str) {
+    std::string target = str.substr(str.find("{") + 1, str.find("}") - str.find("{") - 1);
     return trim(target);
   }
 
   // Finds a substring in a string contained within one specified delimiter
-  std::string findString(std::string str, std::string startDelimiter) {
-    std::size_t startIndex = str.find(startDelimiter);
+  static std::string getSubstr(std::string str, std::string delimiter) {
+    std::size_t startIndex = str.find(delimiter);
     std::size_t stopIndex = str.length();
     std::string target = str.substr(startIndex + 1, startIndex - stopIndex - 1);
     return trim(target);
   }
 
-  /// Splits a string by delimiter
-  // @ref: https://stackoverflow.com/questions/14265581/parse-split-a-string-in-c-using-string-delimiter-standard-c
-  std::vector<std::string> splitString(std::string str, std::string delimiter) {
-    std::vector<std::string> vec;
-    std::size_t index = 0;
-    while ((index = str.find(delimiter)) != std::string::npos) {
-      vec.push_back(str.substr(0, index));
-      str.erase(0, index + delimiter.length());
+  // Splits a comma separated string and returns a set of corresponding integer values
+  static std::set<int> splitStrToInt(std::string str) {
+    std::set<int> aSet;
+    int temp;
+
+    while (!str.empty()) {
+      if (str.find(",") != std::string::npos) {
+        temp = std::stoi(str.substr(str.find_last_of(",") + 1));
+        str.erase(str.find_last_of(","));
+      } else {
+        temp = std::stoi(str);
+        str = "";
+      }
+
+      aSet.insert(temp);
     }
-    vec.push_back(str);
-    return vec;
+
+    return aSet;
   }
 
-  // Converts a string vector to int vector type
-  std::vector<int> stoiVec(std::vector<std::string> strVec) {
-    std::vector<int> intVec;
-    for (std::size_t i = 0; i < strVec.size(); i++) {
-      intVec.push_back(std::stoi(strVec[i]));
-    }
-    return intVec;
+  // Parses the transition matrix to determine the states connected to this state by the epsilon transition.
+  static std::set<int> eClosure(std::unordered_map<int, std::set<int>> transitions, std::set<int> states) {
+    std::set<int> result;
+    eClosureHelper(transitions, states, result);
+    std::cout << "E-closure{";
+    printSet(states);
+    std::cout << "} = {";
+    printSet(result);
+    std::cout << "} = ";
+    return result;
+  }
+
+  // Calculates the set of states that can be obtained from the input symbol 'input'
+  static std::set<int> move(std::unordered_map<int, std::set<int>> transitions, std::set<int> states, std::string input) {
+      std::set<int> result, transition;
+
+      for (int state : states) {
+        transition = transitions.find(state)->second;
+        result.insert(transition.begin(), transition.end());
+      }
+
+      if (!result.empty()) {
+        std::cout << "{";
+        printSet(states);
+        std::cout << "} --" << input << "--> {";
+        printSet(result);
+        std::cout << "}\n";
+      }
+
+      return result;
   }
 
   // Prints out all the elements in a vector
-  void printVec(std::vector<int> vec){
-    std::cout <<  "{";
-    for (std::size_t i = 0; i < vec.size(); i++) {
-      std::cout << vec[i];
-      if (i + 1 != vec.size())
-        std::cout << ", ";
-      else
-        std::cout << "}";
+  static void printSet(std::set<int> set) {
+    std::size_t i = 0;
+    for (int s : set) {
+      std::cout << s;
+      if (i != set.size() - 1)
+        std::cout << ",";
+      i++;
     }
   }
+
 
 };
 
--- eecs665/project1/main.cpp
+++ eecs665/project1/main.cpp
@@ -5,38 +5,211 @@
  * @date    10-13-2016
  * @file    main.cpp
  * @desc    Converts an input NFA into its equivalent DFA
- *
- * NOTE: The program assumes that input follows the following structure:
- * Initial State: {1}
- * Final States: {11}
- * Total States: 11
- * State a b E
- * 1 {} {} {2,5}
- * 2 {3} {} {}
- * 3 {} {4} {}
- * ...
  */
 
 #include <iostream>
-#include "Converter.h"
+#include <sstream>
+#include <queue>
+#include <set>
+#include <unordered_map>
+#include <vector>
+
+#include "Utils.h"
+
+#define DEBUG 1
 
 int main(int argc, char** argv){
   std::cout << "\n===================================";
   std::cout << "\n=====   NFA to DFA Converter  =====";
-  std::cout << "\n===================================\n";
+  std::cout << "\n===================================\n\n";
 
-  // Open the file for reading
-  std::ifstream file(argv[1]);
-  std::vector<std::string> input;
+
+  ////////////////////////////////////////////////////
+  //  Variables for NFA Information
+  ////////////////////////////////////////////////////
+
+  int nfaInitialState = 0;
+  int numStates = 0;
+  int numStateSymbols = 0;
+  std::set<int> nfaFinalStates;
+  std::vector<std::string> stateSymbols;
+  std::unordered_map<std::string, std::unordered_map<int, std::set<int>>> nfa;
+
+
+  ////////////////////////////////////////////////////
+  //  Read NFA from standard input
+  ////////////////////////////////////////////////////
+
   std::string line;
+  std::string temp;
+  std::vector<std::string> input;
 
-  // Read the file line by line into a vector
   while(std::getline(std::cin, line)) {
     input.push_back(line);
   }
 
-  Converter converter(input);
-  converter.setup();
-  converter.convert();
-  converter.printResults();
+  // Parse initial state of NFA from first line
+  nfaInitialState = std::stoi(Utils::trimBrackets(input[0]));
+
+  // Parse final states of NFA from second line
+  nfaFinalStates = Utils::splitStrToInt(Utils::trimBrackets(input[1]));
+
+  // Parse the total number of states in NFA from the third line
+  // ignore the words "Total States:" and get the actual number
+  std::stringstream ss1(input[2]);
+  ss1 >> temp >> temp >> temp;
+  numStates = std::stoi(temp);
+
+  // Parse the state symbols from fourth line
+  // ignore the word "State" in the front
+  std::stringstream ss2(input[3]);
+  ss2 >> temp;
+  while (ss2 >> temp) {
+    stateSymbols.push_back(temp);
+    numStateSymbols++;
+  }
+
+  // Parse the transition diagrams from the fifth line and on
+  for (int i = 0; i < numStates; i++) {
+    int stateNum;
+    std::stringstream ss3(input[i + 4]);
+
+    // Get the state number
+    ss3 >> stateNum;
+
+    // Get the transitions for the current state
+    for (std::size_t j = 0; j < stateSymbols.size(); j++) {
+      if (ss3 >> temp) {
+        // Get the transition for the current state symbol
+        std::set<int> transitions = Utils::splitStrToInt(Utils::trimBrackets(temp));
+
+        // Create a pair with the current state number and transitions
+        std::pair<int, std::set<int>> stateTransitionPairs(stateNum, transitions);
+
+        if (nfa.find(stateSymbols[j]) == nfa.end()) {
+          std::unordered_map<int, std::set<int>> nfaSymbolMap;
+          nfa.insert(std::make_pair(stateSymbols[j], nfaSymbolMap));
+        }
+
+        nfa.find(stateSymbols[j])->second.insert(stateTransitionPairs);
+      }
+    }
+  }
+
+
+  ////////////////////////////////////////////////////
+  //  Variables for DFA
+  ////////////////////////////////////////////////////
+
+  std::unordered_map<std::string, std::unordered_map<int, int>> dfa;
+  std::unordered_map<int, std::set<int>> resultingStates;
+  std::queue<std::pair<int, std::set<int>>> queue;
+  std::set<int> dfaFinalStates;
+  std::set<int> tempSet;
+  int dfaStateNum = 0;
+
+
+  ////////////////////////////////////////////////////
+  //  Convert NFA to DFA
+  ////////////////////////////////////////////////////
+
+  // Insert an empty unordered_map for each state symbol in the NFA to the DFA
+  for (std::size_t i = 0; i < stateSymbols.size() - 1; i++) {
+    std::unordered_map<int, int> dfaSymbolMap;
+    dfa.insert(std::make_pair(stateSymbols[i], dfaSymbolMap));
+  }
+
+  // Find the E-Closure of the initial state
+  tempSet.insert(nfaInitialState);
+  tempSet = Utils::eClosure(nfa.find("E")->second, tempSet); dfaStateNum++;
+  std::cout << dfaStateNum << std::endl;
+
+  // Add the E-Closure transition and DFA state to a queue
+  queue.push(std::make_pair(dfaStateNum, tempSet));
+
+  // Mark every state currently in the queue
+  while (!queue.empty()) {
+    // Get the next E-Closure to process
+    std::pair<int, std::set<int>> currClosure = queue.front(); queue.pop();
+    std::cout << "\nMark " << currClosure.first << std::endl;
+
+    for (std::size_t i = 0; i < stateSymbols.size(); i++) {
+      if (stateSymbols[i] != "E") {
+        tempSet = Utils::move(nfa.find(stateSymbols[i])->second, currClosure.second, stateSymbols[i]);
+
+        // If the move produced results besides {}, find the E-closure of it
+        if (!tempSet.empty()) {
+          tempSet = Utils::eClosure(nfa.find("E")->second, tempSet);
+          std::pair<int, int> stateTransititon;
+
+          // Check if tempSet exists in current result set
+          bool stateExists = false;
+          for (std::pair<int, std::set<int>> state : resultingStates) {
+            if (state.second == tempSet) {
+              stateExists = true;
+
+              // Finish printing the 'E-closure' line
+              std::cout << state.first << "\n";
+
+              // Add this connection to the dfa results
+              stateTransititon = std::make_pair(currClosure.first, state.first);
+              break;
+            }
+          }
+
+          // If we encounter a new state of the dfa, add it to the dfa data structure
+          if (!stateExists) {
+            // Finish printing the 'E-closure' line
+            dfaStateNum++;
+            std::cout << dfaStateNum << "\n";
+            std::pair<int, std::set<int>> newResultState(dfaStateNum, tempSet);
+            resultingStates.insert(newResultState);
+            queue.push(newResultState);
+
+            // If this new state contains a final state, add this to dfa final states
+            for (int state : nfaFinalStates) {
+              if (tempSet.find(state) != tempSet.end()) {
+                dfaFinalStates.insert(dfaStateNum);
+                break;
+              }
+            }
+
+            // Add this connection to the dfa results
+            stateTransititon = std::make_pair(currClosure.first, dfaStateNum);
+          }
+
+          dfa.find(stateSymbols[i])->second.insert(stateTransititon);
+        }
+      }
+    }
+  }
+
+  std::cout << "\nInitial State: {" << nfaInitialState << "}\n";
+  std::cout << "Final states: {";
+  Utils::printSet(dfaFinalStates);
+  std::cout << "}\n";
+  std::cout << "State\t";
+
+  for (std::size_t i = 0; i < stateSymbols.size() - 1; i++) {
+    std::cout << stateSymbols[i] << "\t";
+  }
+
+  for (int i = 1; i <= dfaStateNum; i++) {
+    std::cout << "\n" << i << "\t";
+
+    for (int j = 0; j < stateSymbols.size() - 1; j++) {
+      std::unordered_map<int, int> dfaTransition = dfa.find(stateSymbols[j])->second;
+      if (dfaTransition.find(i) == dfaTransition.end()) {
+          std::cout << "{}\t";
+      } else {
+          int stateData = dfaTransition.find(i)->second;
+          std::cout << "{" << stateData << "}";
+          std::cout << "\t";
+      }
+    }
+  }
+
+  std::cout << std::endl;
+
+  return 0;
 }
