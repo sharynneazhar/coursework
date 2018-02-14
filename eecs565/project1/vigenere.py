45d406d7129e389f63858297e3a3d0f5bebb8112
Sharynne Azhar <sharynneazhar@gmail.com>
2018-02-13 23:07:00 -0600

    updates

--- eecs565/project1/vigenere.py
+++ eecs565/project1/vigenere.py
@@ -1,20 +1,20 @@
+from __future__ import print_function
 import string
 import time
 import itertools
 
-LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-ETAOIN = 'ETAOINSHRDLCUMWFGYPBVKJXQZ'
+LETTERS = string.ascii_uppercase
 
 def vigenere(message, key):
   """Runs the vigenere cipher and returns decrypted text"""
   decrypted_text = ''
-  
+
   # create tuple of message and key
-  mk_tuples = zip(message, itertools.cycle(key))
+  message_key_tuple = zip(message, itertools.cycle(key))
 
   # generate vigenere result
-  for mk_tuple in mk_tuples:
-    loc = reduce(lambda x, y: LETTERS.index(x) - LETTERS.index(y), mk_tuple)
+  for message_key_tuple in message_key_tuple:
+    loc = reduce(lambda x, y: LETTERS.index(x) - LETTERS.index(y), message_key_tuple)
     decrypted_text += LETTERS[loc % 26]
   
   return decrypted_text
@@ -22,8 +22,12 @@ def vigenere(message, key):
 
 def find_key(key_len, word, dict):
   """Finds the decryption key based on brute force dictionary attack"""
-  possible_keys = [''.join(l) for l in itertools.product(LETTERS, repeat = key_len)]
-  print 'Possible Keys: %i' % len(possible_keys)
+  # generate a list of all possible keys based on key length
+  possible_keys = [''.join(i) for i in itertools.product(LETTERS, repeat=key_len)]
+  num_possible_keys = len(possible_keys)
+  print('Possible Keys: %i' % num_possible_keys) 
+
+  # iterate over possible keys and crack the code (INEFFICIENT!)
   for possible_key in possible_keys:
     if vigenere(word, possible_key) in dict:
       return possible_key
@@ -31,39 +35,33 @@ def find_key(key_len, word, dict):
 
 def decrypt(message, key_len, first_word_len, dictionary):
   """Decrypts an encrypted message and returns the decrypted text"""  
-  print '\nMessage: %s' % message
-  print 'Key Length: %i' % key_len
-  print 'First Word Length: %i' % first_word_len
+  print('\nMessage: %s' % message)
+  print('Key Length: %i' % key_len)
+  print('First Word Length: %i' % first_word_len)
 
   # get the first words possibilities based on first word length
-  first_words_list = list(filter(lambda word: len(word) == first_word_len, dictionary))
-  print 'Possible First Words: %i' % len(first_words_list)
+  first_words_list = filter(lambda word: len(word) == first_word_len, dictionary)
+  print('Possible First Words: %i' % len(first_words_list))
 
   # get encrypted first word
-  encrypted_first_word = message[0:first_word_len]
+  first_word = message[0:first_word_len]
 
   # find the decryption key
-  key = find_key(key_len, encrypted_first_word, first_words_list)
+  start = time.time()
+  key = find_key(key_len, first_word, first_words_list)
+  end = time.time()
+  find_key_time = end - start
 
   # decrypt the text
-  text = vigenere(message, key)
+  start = time.time()
+  plaintext = vigenere(message, key)
+  end = time.time()
+  vignere_time = end - start
 
-  print '>> Key: %s' % key
-  print '>> Decrypted Text: %s' % text
-
-
-def run_tests(dictionary):
-  with open('test_cases.txt') as test_file:
-    test_cases = test_file.readlines()
-  
-  test_cases = map(lambda line: line.strip('\n'), test_cases)
-
-  for test in test_cases:
-    test = test.split()
-    start = time.time()
-    decrypt(test[0], int(test[1]), int(test[2]), dictionary)
-    end = time.time()
-    print '>> Time Taken: %.4f seconds' % (end - start)
+  print('>> Key: %s' % key)
+  print('>> Decrypted Text: %s' % plaintext)
+  print('>> Time Taken to Find Key: %.4f seconds' % find_key_time)
+  print('>> Time Taken to Decipher: %.4f seconds\n' % vignere_time)
 
 
 if __name__ == '__main__':
@@ -71,27 +69,16 @@ if __name__ == '__main__':
   dictionary = []
 
   # read the dictionary contents into a list
-  with open('dict.txt') as dict_file:
-    dictionary = dict_file.readlines()
+  with open('dict.txt') as d_file:
+    dictionary = d_file.readlines()
+  dictionary = map(lambda d: d.strip(), dictionary)
+  
+  # read test cases provided into a list
+  with open('test_cases.txt') as test_file:
+    test_cases = test_file.readlines()
+  test_cases = map(lambda tc: tc.strip('\n'), test_cases)
 
-  # strip whitespaces if any
-  dictionary = list(map(lambda line: line.strip(), dictionary))
+  for test in test_cases:
+    test = test.split()
+    decrypt(test[0], int(test[1]), int(test[2]), dictionary)
 
-  # get user input
-  print 'Menu'
-  print '1) Run Tests'
-  print '2) Decode Your Own'
-
-  choice = int(input('Your choice: '))
-  if choice == 1:
-    run_tests(dictionary)
-  else:
-    message = raw_input('Enter your message: ').upper()
-    key_len = int(input('Enter the decryption key length: '))
-    first_word_len = int(input('Enter the first word length: '))
-
-    # run the decryption
-    start = time.time()
-    decrypt(message, key_len, first_word_len, dictionary)
-    end = time.time()
-    print '>> Time Taken: %f' % (end - start)
