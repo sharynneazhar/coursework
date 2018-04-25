3f7b88acba15876dad764e99c978126ede93e8cc
Sharynne Azhar <sharynneazhar@gmail.com>
2018-04-25 10:52:00 -0500

    use test cases from perry

--- eecs662/project3/test.hs
+++ eecs662/project3/test.hs
@@ -3,128 +3,34 @@
 -- Imports for Monads
 import Control.Monad
 
--- Imports for Parser
-import Text.ParserCombinators.Parsec
-import Text.ParserCombinators.Parsec.Language
-import Text.ParserCombinators.Parsec.Expr
-import Text.ParserCombinators.Parsec.Token
-
--- Imports for PLIH
-import ParserUtils
-
 -- Import p3 stuff
 import P3
 
--- Parser magic
-expr :: Parser CFAE
-expr = buildExpressionParser opTable term
+-- Test Cases
 
-opTable = [ [ inFix "+" Plus AssocLeft ,
-              inFix "-" Minus AssocLeft ] ]
+-- Tests for evalDynCFAE and evalStatCFAE.  test2 and test3 should demonstrate
+-- the difference between static and dynamic scoping.  If you get the same
+-- results with both interpreters, you've got problems.
+test0 = (App (Lambda "inc" (Id "inc")) (Lambda "x" (Plus (Id "x") (Num 1))))
+test1 = (App (Lambda "inc" (App (Id "inc") (Num 3))) (Lambda "x" (Plus (Id "x") (Num 1))))
+test2 = (App (Lambda "n" (App (Lambda "inc" (App (Lambda "n" (App (Id "inc") (Num 3))) (Num 3))) (Lambda "x" (Plus (Id "x") (Id "n"))))) (Num 1))
+test3 = (App (Lambda "Sum" (App (Id "Sum") (Num 3))) (Lambda "x" (If0 (Id "x") (Num 0) (Plus (Id "x") (App (Id "Sum") (Minus (Id "x") (Num 1)))))))
 
-numExpr :: Parser CFAE
-numExpr = do i <- integer lexer
-             return (Num (fromInteger i))
+-- List of tests if you would like to use map for testing
+tests = [test0,test1,test2,test3]
 
-lambdaExpr :: Parser CFAE
-lambdaExpr = do reserved lexer "lambda"
-                i <- identifier lexer
-                reserved lexer "in"
-                e <- expr
-                return (Lambda i e)
+-- Tests for evalCFBAE and evalDynCFAE.  These are the same tests as above
+-- using Bind.  You should get the same results from evalCFBAE that you
+-- get from evalStatCFAE.
+test0' = (Bind' "inc" (Lambda' "x" (Plus' (Id' "x") (Num' 1))) (Id' "inc"))
+test1' = (Bind' "inc" (Lambda' "x" (Plus' (Id' "x") (Num' 1))) (App' (Id' "inc") (Num' 3)))
+test2' = (Bind' "n" (Num' 1) (Bind' "inc" (Lambda' "x" (Plus' (Id' "x") (Id' "n"))) (Bind' "n" (Num' 3) (App' (Id' "inc") (Num' 3)))))
+test3' = (Bind' "Sum" (Lambda' "x" (If0' (Id' "x") (Num' 0) (Plus' (Id' "x") (App' (Id' "Sum") (Minus' (Id' "x") (Num' 1)))))) (App' (Id' "Sum") (Num' 3)))
 
-appExpr :: Parser CFAE
-appExpr = do reserved lexer "app"
-             e1 <- expr
-             e2 <- expr
-             return (App e1 e2)
+-- List of tests if you would like to use map for testing
+tests' = [test0',test1',test2',test3']
 
-identExpr :: Parser CFAE
-identExpr = do i <- identifier lexer
-               return (Id i)
-
-ifExpr :: Parser CFAE
-ifExpr = do reserved lexer "if"
-            c <- expr
-            reserved lexer "then"
-            t <- expr
-            reserved lexer "else"
-            e <- expr
-            return (If0 c t e)
-
-term = parens lexer expr
-       <|> numExpr
-       <|> lambdaExpr
-       <|> appExpr
-       <|> identExpr
-       <|> ifExpr
-
--- Parser invocation
-parseCFAE = parseString expr
-
-
-expr' :: Parser CFBAE
-expr' = buildExpressionParser opTable' term'
-
-opTable' = [ [ inFix "+" Plus' AssocLeft ,
-               inFix "-" Minus' AssocLeft ] ]
-
-numExpr' :: Parser CFBAE
-numExpr' = do i <- integer lexer
-              return (Num' (fromInteger i))
-
-lambdaExpr' :: Parser CFBAE
-lambdaExpr' = do reserved lexer "lambda"
-                 i <- identifier lexer
-                 reserved lexer "in"
-                 e <- expr'
-                 return (Lambda' i e)
-
-appExpr' :: Parser CFBAE
-appExpr' = do reserved lexer "app"
-              e1 <- expr'
-              e2 <- expr'
-              return (App' e1 e2)
-
-bindExpr' :: Parser CFBAE
-bindExpr' = do reserved lexer "bind"
-               i <- identifier lexer
-               reservedOp lexer "="
-               v <- expr'
-               reserved lexer "in"
-               e <- expr'
-               return (Bind' i v e)
-
-identExpr' :: Parser CFBAE
-identExpr' = do i <- identifier lexer
-                return (Id' i)
-
-ifExpr' :: Parser CFBAE
-ifExpr' = do reserved lexer "if"
-             c <- expr'
-             reserved lexer "then"
-             t <- expr'
-             reserved lexer "else"
-             e <- expr'
-             return (If0' c t e)
-
-term' = parens lexer expr'
-        <|> numExpr'
-        <|> lambdaExpr'
-        <|> appExpr'
-        <|> bindExpr'
-        <|> identExpr'
-        <|> ifExpr'
-
--- Parser invocation
-parseCFBAE = parseString expr'
-
--- P3 Tests
-interpDyn :: String -> (Maybe CFAE)
-interpDyn = (evalDynCFAE []) . parseCFAE
-
-interpStat :: String -> (Maybe CFAEValue)
-interpStat = (evalStatCFAE []) . parseCFAE
-
-interpCFBAE :: String -> (Maybe CFAEValue)
-interpCFBAE = (evalCFBAE []) . parseCFBAE
+runTests =
+    do putStrLn $ ("evalDynCFAE  -- " ++ (show $ map (evalDynCFAE []) tests))
+       putStrLn $ ("evalStatCFAE -- " ++ (show $ map (evalStatCFAE []) tests))
+       putStrLn $ ("evalCFBAE    -- " ++ (show $ map (evalCFBAE []) tests'))
